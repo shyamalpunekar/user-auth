ca22afec688da5e8173b95169832cccfdc103f3a
origin <shyamal.punekar@gmail.com>
2017-11-30 16:03:00 -0800

    Add firebase setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyAp0s2wuxOz3dyuBQHm34AZED9CoLcIx2U",
+        authDomain: "authentication-reac.firebaseapp.com",
+        databaseURL: "https://authentication-reac.firebaseio.com",
+        projectId: "authentication-reac",
+        storageBucket: "authentication-reac.appspot.com",
+        messagingSenderId: "619836024703"
+    });
+  }
   render() {
     return (
       <View>
