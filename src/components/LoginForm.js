62e54300ba80f9ab9b6200ddce770df46932e4bd
origin <shyamal.punekar@gmail.com>
2017-12-01 14:01:17 -0800

    Add error handling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state= { email: '', password: ''};
+  state= { email: '', password: '', error: ''};
+
+  onButtonPress(){
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword()
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(() => {
+          this.setState({ error: 'Authentication Failed' })
+        });
+    });
+  }
   render() {
     return (
       <Card>
@@ -21,12 +34,16 @@ class LoginForm extends Component {
             placeholder="password"
             label="Password"
             value={this.state.password}
-            onChangeText={password => this.setState({ password })}            
+            onChangeText={password => this.setState({ password })}
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
           Log in
           </Button>
         </CardSection>
@@ -34,5 +51,13 @@ class LoginForm extends Component {
     )
   }
 }
+
+const styles = {
+  errorTextStyle:{
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
