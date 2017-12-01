a9d17f19121a7caa6c8d00a531800ae0f32c5d4a
origin <shyamal.punekar@gmail.com>
2017-11-30 16:15:07 -0800

    Add login form

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm  from './components/LoginForm'
 
 
 class App extends Component {
@@ -19,7 +20,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App</Text>
+        <LoginForm />
       </View>
     );
   }
