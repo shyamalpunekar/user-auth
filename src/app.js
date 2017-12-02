b02ec2313003c4ab40d7b47887c387c1e4450c47
origin <shyamal.punekar@gmail.com>
2017-12-01 16:23:08 -0800

    Add logout functioanlity

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ class App extends Component {
       case true:
       return (
         <CardSection>
-          <Button>
+          <Button onPress={() => firebase.auth().signOut()}>
             Log Out
           </Button>
         </CardSection>
